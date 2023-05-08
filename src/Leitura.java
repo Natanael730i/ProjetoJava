d70dd506660682d5ed3208ced7ea62ae42890e55
Natanael730i <natanaelmuller730@gmail,com>
2023-05-08 12:41:27 -0300

    Scanner

--- src/Leitura.java
+++ src/Leitura.java
@@ -6,7 +6,13 @@ public class Leitura {
 
         System.out.println("Digite seu Filme favorito: ");
         String filme = leitura.nextLine();
+        System.out.println("Qual é o ano de lançamento: ");
+        int ano = leitura.nextInt();
+        System.out.println("Diga a sua avaliação para o filme");
+        double avaliacao = leitura.nextDouble();
 
-        System.out.println(filme);
+        System.out.println("Filme informado: " + filme);
+        System.out.println("Ano de Lançamento informado: " + ano);
+        System.out.println("Avaliação informada: " + avaliacao);
     }
 }
