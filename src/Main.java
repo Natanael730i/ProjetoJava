f02364685e492cd2760448b4a593ae822bb0f083
Natanael730i <natanaelmuller730@gmail,com>
2023-05-07 23:54:48 -0300

    Projeto inicial

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,18 @@ public class Main {
         Ano de lançamento:
         """ + ano;
         System.out.println(Sinopse);
+
+        String nome = "Natanael";
+        Integer idade = 26;
+        Double valorGasto = 6.0;
+        System.out.println("\n");
+        System.out.println(String.format("Meu nome é %s, eu tenho %d " +
+                "anos e hoje gastei %.2f reais", nome, idade, valorGasto));
+
+//        cast explcito
+        double classificacao = media/2;
+
+        System.out.println(classificacao);
+
     }
 }
