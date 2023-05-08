a7d3de4271e16e8e9b8c055f37aa1f8a78a27550
Natanael730i <natanaelmuller730@gmail,com>
2023-05-07 23:36:02 -0300

    Projeto inicial

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Este é o Screen Match");
+        System.out.println("Filme: Top Gam: Maverick");
+
+        Integer ano = 2022;
+        //Média calculada pelas 3 notas do filme
+        /*
+        * Comentário de mais de uma linha
+        */
+        double media = (9.8 + 6.2 + 8.0)/3;
+
+        System.out.println(media);
+
+        String Sinopse = """
+        Filme Top Gun
+        Filme de aventura com galâ dos anos 80
+        Muito bom!
+        Ano de lançamento: 
+        """ + ano;
+        System.out.println(Sinopse);
+    }
+}
